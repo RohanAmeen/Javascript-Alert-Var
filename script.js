7a38a29028109a1a463dfc7f721a22eabd325f2b
RohanAmeen <rohanameen55@gmail.com>
2023-09-01 17:26:45 +0500

    Javascript Alert & Variables Tasks

---
+++ script.js
@@ -0,0 +1,66 @@
+// CHAPTER NO 1      
+//  Q NO 1
+
+// alert("HELLO WORLD");
+
+// Q NO 2
+//alert("Error! Please Enter a valid password");
+
+//Q No 3
+
+//alert("Welcome to JS Land...\nHappy Coding!");
+
+//Q No 4
+
+  // alert("Welcome to JS Land...");
+  // alert("Happy Coding!");
+
+  //Q No 5
+// alert("Hello...i can run js through my web browser's console");
+
+
+
+// CHAPTER NO 2
+//  Q No 1
+//var username;
+
+//Q No 2
+//var myName="Rohan Ameen";
+
+//Q No 3
+// var message;
+// message ="Hello World";
+// alert(message);
+
+//Q No 4
+// var studentName="Johne Doe";
+// alert(studentName);
+// var age ="15 Years old";
+// alert(age);
+// var skill ="Certified Mobile Application Development";
+// alert(skill);
+
+
+//Q No 5
+
+//var message ="PIZZA\nPIZZ\nPIZ\nPI\nP";
+//alert(message);
+
+//Q No 6
+
+//var email ="rohanameen55@gmail.com";
+//alert("My Email Address Is "+email);
+
+//Q No 7
+
+//var book ="A Smarter\nWay to learn javascript";
+//alert("I am trying to learn from the book "+book);
+
+//Q No 8
+//document.write("YAH! I CAN WRITE HTML Content Through Javascript");
+
+//Q No 9
+
+//var message = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+//alert(message);
+//document.write(message);
